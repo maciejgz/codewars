14098737178992735b3964ceb5e5e033d4801aa3
mgzik <mgzik@ospartner.pl>
2016-07-06 19:38:42 +0200

    nieskończony wind

---
+++ codewars/src/main/java/pl/mg/codewars/WindCalculator.java
@@ -0,0 +1,36 @@
+package pl.mg.codewars;
+
+/**
+ * When landing an airplane manually, the pilot knows which runway he is using and usually has up to date wind information (speed and direction). This information alone does not help the pilot make a safe landing; what the pilot really needs to know is the speed of headwind, how much crosswind there is and from which side the crosswind is blowing relative to the plane.
+ * <p/>
+ * Let's imagine there is a system in the ATC tower with speech recognition that works so that when a pilot says "wind info" over the comms, the system will respond with a helpful message about the wind.
+ * <p/>
+ * Your task is to write a function that produces the response before it is fed into the text-to-speech engine.
+ * <p/>
+ * Input:
+ * <p/>
+ * runway (string: "NN[L/C/R]"). NN is the runway's heading in tens of degrees. A suffix of L, C or R may be present and should be ignored. NN is between 01 and 36.
+ * wind_direction (int). Direction wind is blowing from in degrees. Between 0 and 359.
+ * wind_speed (int). Wind speed in knots
+ * Output:
+ * <p/>
+ * a string in the following format: "(Head|Tail)wind N knots. Crosswind N knots from your (left|right)."
+ * The wind speeds must be correctly rounded integers. If the rounded headwind component is 0, "Head" should be used. Similarly, "right" in case crosswind component is 0.
+ * <p/>
+ * Calculating crosswind and headwind:
+ * <p/>
+ * A = Angle of the wind from the direction of travel (radians)
+ * WS = Wind speed
+ * CW = Crosswind
+ * HW = Headwind
+ * <p/>
+ * CW = sin(A) * WS
+ * HW = cos(A) * WS
+ * Created by Maciej Gzik on 2015-07-01.
+ */
+public class WindCalculator {
+
+    public static void main(String[] args){
+        System.out.println("wind");
+    }
+}
